6f554a87a4635c2e2b62289f37df2da9578948b1
Harendra Prasad <harendraprasadtest@gmail.com>
2025-07-27 21:35:09 +0530

    added action yaml

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage123"
-    container_name       = "tfstate"
-    key                  = "aks/terraform.tfstate"
+    resource_group_name  = "poc001"
+    storage_account_name = "stgactdewas"
+    container_name       = "tfstate-dev"
+    key                  = "terraform.tfstate"
   }
 }

5ec86ea94cf78baa706ed06d4b79c932224eade0
Harendra Prasad <harendraprasadtest@gmail.com>
2025-07-28 00:16:29 +0530

    added code with dockerfile & aks manifests

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from AKS!"
+def home():
+    return "Hello from Python App running on AKS!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host="0.0.0.0", port=80)
